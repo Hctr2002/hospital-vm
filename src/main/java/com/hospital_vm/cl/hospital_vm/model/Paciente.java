be01637eb01a4759b70cee77585241f803bbc716
Héctor <he.robledo@duocuc.cl>
2025-05-07 09:56:41 -0400

    First commit

---
+++ src/main/java/com/hospital_vm/cl/hospital_vm/model/Paciente.java
@@ -0,0 +1,41 @@
+package com.hospital_vm.cl.hospital_vm.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique=true, length=13, nullable=false)
+    private String run;
+
+    @Column(nullable=false)
+    private String nombres;
+
+    @Column(nullable=false)
+    private String apellidos;
+
+    @Column(nullable=true)
+    private Date fechaNacimiento;
+    
+    @Column(nullable=false)
+    private String correo;
+
+}
